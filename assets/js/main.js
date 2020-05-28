8a4eaace066d955e6019d50d9df17560ca52a2aa
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-05-28 22:45:55 +0700

    update masalah token

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000'
+const baseUrl = 'https://icybe.aliven.my.id/'
 
 grecaptcha.ready(async function() {
     const token = await grecaptcha.execute('6LeatvwUAAAAAANgMTBjt-eD0NkSZu2eyoaUExju', {action: 'submit'})
